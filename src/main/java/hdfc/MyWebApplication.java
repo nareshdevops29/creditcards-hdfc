28e56ebda92989e7ce912fa64d815403c0c2a3ac
Your Name <naresh.29devops@gmail.com>
2021-05-06 17:42:55 +0530

    new code developed for hdfc

---
+++ src/main/java/hdfc/MyWebApplication.java
@@ -0,0 +1,16 @@
+package hdfc;
+
+import org.orienteer.core.OrienteerWebApplication;
+import org.orienteer.core.module.PerspectivesModule;
+
+public class MyWebApplication extends OrienteerWebApplication
+{
+	@Override
+	public void init()
+	{
+		super.init();
+		mountPages("hdfc.web");
+		registerModule(DataModel.class);
+	}
+	
+}
